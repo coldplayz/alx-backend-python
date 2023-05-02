7d2ecfed763b2f32dfc09f4be2aa97d44b434609
greenbel <obisann@gmail.com>
2023-05-02 11:46:35 +0000

    0x01-T1: testing...

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 ''' Fun with Async IO using asyncio package. '''
 import asyncio
-from typing import Any
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -11,11 +10,13 @@ async def wait_n(n: int, max_delay: int = 10):
     ''' Spawns @wait_random @n times.
     '''
     # print(*[7 for _ in range(n)])
-    # return await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+    return await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
 
+    '''
     durations = []
     for _ in range(n):
         duration = await asyncio.gather(wait_random(max_delay))
         durations.extend(duration)
 
     return durations
+    '''
