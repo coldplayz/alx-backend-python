1ef02391c784add694a81b6459bb802287d365b6
greenbel <obisann@gmail.com>
2023-05-02 13:56:27 +0000

    0x01-T3: init

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+''' Fun with Async IO using asyncio package. '''
+import asyncio
+# from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    ''' Returns an asyncio.Task object using @max_delay.
+    '''
+    coroutine = wait_random(max_delay)  # collect a coro
+    task = asyncio.create_task(coroutine)  # wrap a coro in a Task
+    return task
