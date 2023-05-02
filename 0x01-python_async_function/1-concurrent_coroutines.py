565dd4fd34a0d86e94b594b3838ecc3fb7ba5f6f
greenbel <obisann@gmail.com>
2023-05-02 12:18:22 +0000

    0x01-T1: added return annotation.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 ''' Fun with Async IO using asyncio package. '''
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 # A co-ordinating coroutine
-async def wait_n(n: int, max_delay: int = 10):
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     ''' Spawns @wait_random @n times.
     '''
     # print(*[7 for _ in range(n)])
