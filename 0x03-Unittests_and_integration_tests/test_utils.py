37f18c69d6deaad32dc2db5c7324572b7e7b6560
greenbel <obisann@gmail.com>
2023-05-20 10:44:51 +0000

    0x03: changed test method name

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,6 +17,6 @@ class TestAccessNestedMap(TestCase):
     """Implementation class for testing access_nested_map function.
     """
     @parameterized.expand(INPUTS)
-    def test_return(self, nested_map: Mapping, path: Sequence, expected: Any) -> None:
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any) -> None:
         """Test the function's return."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
